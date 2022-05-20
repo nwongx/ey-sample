6b3d80152a0516dcc6a9f3ccf544d984c06da0af
nick <n.wongx@gamil.com>
2022-05-20 15:59:17 -0400

    refactor: transfer confirm data

--- src/pages/Transfer.tsx
+++ src/pages/Transfer.tsx
@@ -1,10 +1,11 @@
 import { Box, Divider, Grid, Typography } from '@mui/material';
-import React, { FC, useContext, useState } from 'react';
+import React, { FC, useCallback, useContext, useState } from 'react';
 import { WalletContext } from '../contexts/walletContext';
 import TransferForm from '../components/TransferForm';
 import type { IFormData } from '../components/TransferForm';
 import { simplifiedBEP20Abi } from '../data/abi';
 import TransferConfirmModal from '../components/TransferConfirmModal';
+import { IDisplayContent } from '../components/TransferInfoRow';
 
 interface IRawTx {
   from?: string;
@@ -32,6 +33,9 @@ const formHeaderStyles = {
 
 const Transfer: FC = function () {
   const [transferParam, setTransferParam] = useState<IFormData | null>(null);
+  const [confirmInfo, setConfirmInfo] = useState<IDisplayContent[] | null>(
+    null
+  );
   const [open, setOpen] = useState(true);
   const { web3, accounts } = useContext(WalletContext);
 
@@ -48,97 +52,98 @@ const Transfer: FC = function () {
       handleOpen();
     }
     setTransferParam(formData);
+    setConfirmInfo([
+      {
+        title: 'To',
+        content: formData.toAddress as string,
+      },
+      {
+        title: 'Token address',
+        content: formData.tokenAddress as string,
+      },
+      {
+        title: 'Amount',
+        content: formData.amount as string,
+      },
+      {
+        title: 'Estimated Gas',
+        content: formData.estimatedGas as number,
+      },
+    ]);
   }
 
-  function onConfirmHandler() {
-    async function sendTransaction() {
-      if (transferParam && accounts) {
-        let rawTx: IRawTx = {};
-        try {
-          if (transferParam.tokenAddress === 'native') {
-            rawTx = {
-              from: accounts[0],
-              to: transferParam.toAddress as string,
-              value: web3.utils.toHex(transferParam.amount as string),
-              chainId: process.env.REACT_APP_CHAIN_ID as string,
-            };
-          } else {
-            const contract = new web3.eth.Contract(
-              simplifiedBEP20Abi,
-              transferParam.tokenAddress as string,
-              {
+  const onConfirmHandler = useCallback(
+    function () {
+      async function sendTransaction() {
+        if (transferParam && accounts) {
+          let rawTx: IRawTx = {};
+          try {
+            if (transferParam.tokenAddress === 'native') {
+              rawTx = {
                 from: accounts[0],
-              }
-            );
-            /* eslint-disable @typescript-eslint/no-unsafe-call,  
+                to: transferParam.toAddress as string,
+                value: web3.utils.toHex(transferParam.amount as string),
+                chainId: process.env.REACT_APP_CHAIN_ID as string,
+              };
+            } else {
+              const contract = new web3.eth.Contract(
+                simplifiedBEP20Abi,
+                transferParam.tokenAddress as string,
+                {
+                  from: accounts[0],
+                }
+              );
+              /* eslint-disable @typescript-eslint/no-unsafe-call,  
         @typescript-eslint/no-unsafe-member-access,  
         @typescript-eslint/no-unsafe-assignment */
-            const data: string = await contract.methods
-              .transfer(
-                transferParam.toAddress as string,
-                transferParam.amount as string
-              )
-              .encodeABI();
+              const data: string = await contract.methods
+                .transfer(
+                  transferParam.toAddress as string,
+                  transferParam.amount as string
+                )
+                .encodeABI();
+              /* eslint-enable @typescript-eslint/no-unsafe-call,  
+        @typescript-eslint/no-unsafe-member-access,  
+        @typescript-eslint/no-unsafe-assignment */
+              rawTx = {
+                from: accounts[0],
+                to: transferParam.tokenAddress as string,
+                data,
+                chainId: process.env.REACT_APP_CHAIN_ID as string,
+              };
+            }
+            await window.ethereum.request({
+              method: 'eth_sendTransaction',
+              params: [rawTx],
+            });
 
-            /* eslint-enable @typescript-eslint/no-unsafe-call,  
-        @typescript-eslint/no-unsafe-member-access,  
-        @typescript-eslint/no-unsafe-assignment */
-            rawTx = {
-              from: accounts[0],
-              to: transferParam.tokenAddress as string,
-              data,
-              chainId: process.env.REACT_APP_CHAIN_ID as string,
-            };
+            // todo: redirect to explore
+          } catch (e) {
+            // todo: error handling
+            console.log(e);
+          } finally {
+            handleClose();
           }
-          await window.ethereum.request({
-            method: 'eth_sendTransaction',
-            params: [rawTx],
-          });
-
-          // todo: redirect to explore
-        } catch (e) {
-          // todo: error handling
-          console.log(e);
-        } finally {
-          handleClose();
         }
       }
-    }
 
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    sendTransaction();
-  }
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      sendTransaction();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [accounts, transferParam]
+  );
 
   return (
     <>
-      {transferParam?.toAddress &&
-        transferParam?.tokenAddress &&
-        transferParam.amount &&
-        transferParam.estimatedGas && (
-          <TransferConfirmModal
-            open={open}
-            onClose={handleClose}
-            onConfirm={onConfirmHandler}
-            displayContent={[
-              {
-                title: 'To',
-                content: transferParam.toAddress,
-              },
-              {
-                title: 'Token address',
-                content: transferParam.tokenAddress,
-              },
-              {
-                title: 'Amount',
-                content: transferParam.amount,
-              },
-              {
-                title: 'Estimated Gas',
-                content: transferParam.estimatedGas,
-              },
-            ]}
-          />
-        )}
+      {confirmInfo && (
+        <TransferConfirmModal
+          open={open}
+          onClose={handleClose}
+          onConfirm={onConfirmHandler}
+          displayContent={confirmInfo}
+        />
+      )}
       <Grid
         sx={gridStyles}
         container
